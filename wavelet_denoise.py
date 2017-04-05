382055ce1dfb57c87d83e782599b76f24dc94706
tjipenk <tjipenk@gmail.com>
2017-04-05 10:45:01 +0700

    wavelet denois multiprocessor

--- wavelet_denoise.py
+++ wavelet_denoise.py
@@ -4,10 +4,22 @@ Created on Tue Apr  4 06:33:30 2017
 
 @author: titik
 """
+
 import numpy as np
 import matplotlib.pyplot as plt
 import pywt
+from joblib import Parallel, delayed
+import multiprocessing
+    
+# what are your inputs, and what operation do you want to 
+# perform on each input. For example...
+inputs = range(10) 
+def processInput(i):
+	return i * i
+ 
+num_cores = multiprocessing.cpu_count()
 
+    
 def wavelet_trasnform(sig):
     mode = pywt.Modes.smooth
     w = 'coif3'
@@ -33,7 +45,6 @@ def wavelet_trasnform(sig):
 #    hasil.append(rec_a[0])
         
     return rec_a[0];
-        
 
 #fname = '/home/titik/TXT/numlowpass-sawah_2015.tif.txt.cluster_centres'
 fname = '/home/titik/TXT/Sawah_2015.tif.txt'
@@ -41,8 +52,10 @@ data = np.loadtxt(fname)
 sig = data[:,1:24]
 hasil=[]
 #plt.plot(data[:,1:23].transpose())
-for n in range(sig.shape[0]):
-    dd = wavelet_trasnform(sig[n,:].transpose())
-    hasil.append(dd)
-hasil = np.asarray(hasil)
+#for n in range(sig.shape[0]):
+#    dd = wavelet_trasnform(sig[n,:].transpose())
+#    hasil.append(dd)
+#hasil = np.asarray(hasil)
 #plt.plot(hasil[:,0:23].transpose())
+#results = Parallel(n_jobs=num_cores)(delayed(processInput)(i) for i in inputs)
+hasil_par = Parallel(n_jobs=num_cores)(delayed(wavelet_trasnform)(i) for i in sig)
